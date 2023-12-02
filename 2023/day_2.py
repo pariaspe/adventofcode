598816bc8c98b009c0ebc3a89a5ef1d17cb0aed6
pariaspe <pedro.ariasp@upm.es>
2023-12-02 16:28:46 -0600

    day 2 part 2

--- 2023/day_2.py
+++ 2023/day_2.py
@@ -34,6 +34,10 @@ class Game:
         """Check if game is valid"""
         return self.red <= r and self.green <= g and self.blue <= b
 
+    def power_sum(self) -> int:
+        """Return power sum of values"""
+        return self.red * self.green * self.blue
+
 
 def part_one(filename: str, key_input: tuple[int, int, int]) -> int:
     """Solution for part one"""
@@ -45,10 +49,26 @@ def part_one(filename: str, key_input: tuple[int, int, int]) -> int:
             new_game = Game.from_string(line)
             if new_game.is_valid(*key_input):
                 games.append(new_game)
-    return sum(list(map(lambda x: x.index, games)))
+    return sum(game.index for game in games)
+
+
+def part_two(filename: str) -> int:
+    """Solution for part two"""
+    games: list[Game] = []
+    with open(filename, encoding='utf-8') as f:
+        for line in f.readlines():
+            if not line.strip():
+                break
+            new_game = Game.from_string(line)
+            games.append(new_game)
+    return sum(game.power_sum() for game in games)
 
 
 if __name__ == "__main__":
     assert part_one("2-input_test.txt", (12, 13, 14)) == 8
     solution_part_one = part_one("2-input.txt", (12, 13, 14))
     print(f"{solution_part_one=}")
+
+    assert part_two("2-input_test.txt") == 2286
+    solution_part_two = part_two("2-input.txt")
+    print(f"{solution_part_two=}")
