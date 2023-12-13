daf3627a247b94611e413a9949c180ba9ad4ec96
pariaspe <pedro.ariasp@upm.es>
2023-12-13 13:22:35 -0600

    day 8 part 2

--- 2023/day_8.py
+++ 2023/day_8.py
@@ -2,6 +2,8 @@
 Day 8 | Advent of Code 2023
 """
 
+from math import lcm
+
 
 def part_one(filename: str) -> int:
     """Solution for part one"""
@@ -39,14 +41,59 @@ def part_one(filename: str) -> int:
     return i
 
 
+def find_ends(nodes: dict, starting_node: str, instructions: list) -> list[int]:
+    """Find end of network"""
+    i = 0
+    current_node = starting_node
+    end = False
+    end_nodes = []
+    steps = []
+    while not end:
+        for instruction in instructions:
+            if instruction == "R":
+                n = 1
+            elif instruction == "L":
+                n = 0
+
+            current_node = nodes[current_node][n]
+            i += 1
+            if list(current_node)[-1] == 'Z':
+                if current_node in end_nodes:
+                    end = True
+                    break
+                end_nodes.append(current_node)
+                steps.append(i)
+
+    return steps
+
+
 def part_two(filename: str) -> int:
     """Solution for part two"""
+    instructions = []
+    starting_nodes = []
+    nodes = {}
     with open(filename, encoding='utf-8') as f:
         for line in f.readlines():
             if not line.strip():
-                break
+                continue
 
-    return 0
+            if not instructions:
+                instructions = list(line.strip())
+                continue
+
+            node, paths = line.strip().split(" = (")
+            right, left = paths.split(" ")
+            if list(node)[-1] == "A":
+                starting_nodes.append(node)
+            nodes[node] = (right[:-1], left[:-1])
+
+    # print(starting_nodes)
+    ends = {}
+    for node in starting_nodes:
+        ends[node] = find_ends(nodes, node, instructions)
+
+    # print([end[-1] for end in ends.values()])
+    return lcm(*[end[-1] for end in ends.values()])
 
 
 if __name__ == "__main__":
@@ -55,6 +102,6 @@ if __name__ == "__main__":
     solution_part_one = part_one("8.in")
     print(f"{solution_part_one=}")
 
-    assert part_two("8a.ex") == 71503
+    assert part_two("8c.ex") == 6
     solution_part_two = part_two("8.in")
     print(f"{solution_part_two=}")
